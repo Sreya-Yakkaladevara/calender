f8a1b9603281ad6d7a1a79eb8d61bf47c762c30e
Sreya-Yakkaladevara <154442200+Sreya-Yakkaladevara@users.noreply.github.com>
2026-06-30 12:29:59 +0530

    Update vite.config.js

--- my-calendar/vite.config.js
+++ my-calendar/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This forces Vite to listen on 0.0.0.0
+    port: 5173  // Keeps it consistently on 5173
+  }
 })
